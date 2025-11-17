9aa82eef96897e9a0158597b2f8bab7acb7f3493
jcheva123 <125487948+jcheva123@users.noreply.github.com>
2025-11-17 14:14:32 -0300

    Update radar.js

--- api/radar.js
+++ api/radar.js
@@ -1,10 +1,11 @@
 // api/radar.js
 module.exports = async (req, res) => {
   try {
-    // Por ahora: URL fija (podés pegar la última que veas en la web del SMN)
+    // Por ahora usamos una URL fija del radar de Bahía Blanca.
+    // Cuando quieras, la cambiamos para que sea automática.
     const url = 'https://estaticos.smn.gob.ar/vmsr/radar/RMA10_240_ZH_CMAX_20251114_130223Z.png';
 
-    // Cabecera opcional, pero no molesta
+    // Cabecera opcional (no es necesaria, pero no molesta)
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     res.status(200).json({ url });
